925aa14e6417def2b04a2401ac8a137047ff9e42
Anass Al <mr@anassinator.com>
2015-09-28 16:04:06 -0400

    Add basic_node example

---
+++ catkin_ws/src/topics/scripts/basic_node.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Basic ROS node example.
+
+This creates a basic ROS node called 'basic_node' that persists until ROS is
+stopped.
+
+This can be observed by running 'rosnode list'.
+"""
+
+import rospy
+
+
+if __name__ == "__main__":
+    # Initialize node.
+    rospy.init_node("basic_node")
+
+    # Run until stopped.
+    rospy.spin()
